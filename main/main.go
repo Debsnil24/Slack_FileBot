ce8e1a1aed9cf73f1e038f932e1d2d6c2cb80e1a
Debsnil24 <debsnilsamudra@gmail.com>
2025-03-04 17:09:11 +0530

    Implemented the Main function **Summary** - Implemented the main function - Updated the .env file with necessary details - Tested the Bot **Testing** - Tested the bot on a single channel and with a single file - The bot was working as expected **To-Do** - Write Documentation for the project - Work on uploading multiple file to multiple channels

--- main/main.go
+++ main/main.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
-	"github.com/slack-go/slack"
-	"github.com/joho/godotenv"
+	"log"
 	"os"
+	"github.com/joho/godotenv"
+	"github.com/slack-go/slack"
 )
 
 func main() {
+	err := godotenv.Load("/Users/debsnilsamudra/Documents/Program/Slack_FileBot/.env")
+	if err != nil {
+		log.Fatal("Error Loading .env File")
+	}
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelID:= os.Getenv("CHANNEL_ID")
+	filePath := os.Getenv("FILE_PATH")
 
+	fileinfo, err := os.Stat(filePath)
+	if err != nil {
+		log.Fatalf("Error: Unable to get file info %v", err)
+	}
+
+	params := slack.UploadFileV2Parameters{
+		Channel: channelID,
+		File:    filePath,
+		Filename: fileinfo.Name(),
+		FileSize:  int(fileinfo.Size()),
+	}
+
+	file, err := api.UploadFileV2(params)
+	if err != nil {
+		log.Fatalf("Error: Unable to upload file %v", err)
+		return
+	}
+	
+	fmt.Printf("Title: %v, ID: %v\n", file.Title, file.ID)
 }
